b38ac31e459b831fe2b78e00df12be1e43d11aab
Pierre-Yves Ritschard <pyr@spootnik.org>
2018-01-08 12:15:26 +0100

    add deps.edn

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject spootnik/signal "0.2.1"
+(defproject spootnik/signal "0.2.2"
   :description "system signal handler for clojure."
   :url "https://github.com/pyr/signal"
   :license {:name "MIT/ISC License"}
   :jvm-opts ["-Dclojure.compiler.direct-linking=true"]
-  :profiles {:dev {:global-vars {*warn-on-reflection* true}}}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]])
+  :global-vars {*warn-on-reflection* true}
+  :dependencies [[org.clojure/clojure "1.9.0"]])
