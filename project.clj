1dacc6ac05cda85ba38ea5e2a2a3060adbe86248
Pierre-Yves Ritschard <pyr@spootnik.org>
2019-10-13 11:29:33 +0200

    circle: produce junit

--- project.clj
+++ project.clj
@@ -9,9 +9,13 @@
     :license {:name "MIT/ISC License"}
     :jvm-opts ["-Dclojure.compiler.direct-linking=true"]
     :pedantic? :abort
-    :aliases {"kaocha" ["with-profile" "+dev" "run" "-m" "kaocha.runner"]}
+    :aliases {"kaocha" ["with-profile" "+dev" "run" "-m" "kaocha.runner"]
+              "junit"  ["with-profile" "+dev" "run" "-m" "kaocha.runner"
+                        "--plugin" "kaocha.plugin/junit-xml" "--junit-xml-file"
+                        "target/junit/results.xml"]}
     :deploy-repositories [["snapshots" :clojars] ["releases" :clojars]]
-    :profiles {:dev {:dependencies [[lambdaisland/kaocha "0.0-554"]]
+    :profiles {:dev {:dependencies [[lambdaisland/kaocha           "0.0-554"]
+                                    [lambdaisland/kaocha-junit-xml "0.0-70"]]
                      :pedantic?    :warn}}
     :dependencies ~deps
     :source-paths ~paths))
