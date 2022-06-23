6a825f4b0dcf5143c128a090b09f5c038b1322a9
Pierre-Yves Ritschard <pyr@spootnik.org>
2022-06-23 13:02:20 +0200

    Ensure handlers run in a context that has a valid class loader
    Fixes #3

--- src/signal/handler.clj
+++ src/signal/handler.clj
@@ -1,5 +1,16 @@
 (ns signal.handler
-  "Signal handling functions")
+  "Signal handling functions"
+  (:import clojure.lang.Compiler
+           clojure.lang.DynamicClassLoader))
+
+(defn- set-compiler-class-loader
+  "Ensures that the class loader in which a handler will
+   execute runs in a context which can load clojure classes."
+  []
+  (let [thread (Thread/currentThread)]
+    (when-not (instance? DynamicClassLoader (.getContextClassLoader thread))
+      (.setContextClassLoader thread (DynamicClassLoader.
+                                      (.getClassLoader Compiler))))))
 
 (defn ^sun.misc.Signal ->signal
   "Convert a keyword to an appropriate Signal instance."
@@ -20,7 +31,9 @@
   "Convert class to signal handler."
   [handler]
   (proxy [sun.misc.SignalHandler] []
-    (handle [sig] (handler (signal->kw sig)))))
+    (handle [sig]
+      (set-compiler-class-loader)
+      (handler (signal->kw sig)))))
 
 (defn on-signal
   "Execute handler when signal is caught"
